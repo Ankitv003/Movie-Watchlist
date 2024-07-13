a975efd206610925015ff3b9b56c62a077f0c26d
Ankit Verma <ankitv78389@gmail.com>
2024-07-13 15:02:29 +0530

    :hammer:[refractor][final mobile design completed]

---
+++ data.js
@@ -0,0 +1,41 @@
+const data = {
+  Actors: "Chris Pratt, Zoe Saldana, Dave Bautista",
+  Awards: "Nominated for 1 Oscar. 15 wins & 60 nominations total",
+  BoxOffice: "$389,813,101",
+  Country: "United States",
+  DVD: "N/A",
+  Director: "James Gunn",
+  Genre: "Action, Adventure, Comedy",
+  Language: "English",
+  Metascore: "67",
+  Plot: "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father, the ambitious celestial being Ego.",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+  Production: "N/A",
+  Rated: "PG-13",
+  Ratings: [
+    {
+      Source: "Internet Movie Database",
+      Value: "7.6/10",
+    },
+    {
+      Source: "Rotten Tomatoes",
+      Value: "85%",
+    },
+    {
+      Source: "Metacritic",
+      Value: "67/100",
+    },
+  ],
+  Released: "05 May 2017",
+  Response: "True",
+  Runtime: "136 min",
+  Title: "Guardians of the Galaxy Vol. 2",
+  Type: "movie",
+  Website: "N/A",
+  Writer: "James Gunn, Dan Abnett, Andy Lanning",
+  Year: "2017",
+  imdbID: "tt3896198",
+  imdbRating: "7.6",
+  imdbVotes: "764,992",
+};
--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+const movieList = document.querySelector(".movie-list");
+
 fetch("https://www.omdbapi.com/?i=tt3896198&apikey=78687159")
-  .then((res) => res.json)
-  .then((data) => console.log(data));
+  .then((res) => res.json())
+  .then((data) =>
+    console.log(data)(
+      (movieList.innerHTML = `
+        <div class="movie-poster">
+            <img src="${data.Poster}" alt="movie-poster" />
+        </div>
+    <div class="movie-list-container">
+        <div class="movie-list-contents">
+            <div class="movie-name">
+                <h3>${data.Title}</h3>
+                <div class="movie-rating">
+                    <img src="images/imdb-rating.png" alt="star-rating" />
+                    <p>${data.imdbRating}</p>
+                </div>
+            </div>
+            <div class="movie-details">
+                <p>${data.Runtime}</p>
+                <p>${data.Genre}</p>
+            </div>
+            <div class="movie-about">
+                <p>${data.Plot}</p>
+            </div>
+            <div class="add-watchlist">
+                <button>Add to watchlist</button>
+            </div>
+        </div>
+    </div>
+    
+    <hr>
+
+`)
+    )
+  );
